b974564305ec729130a70486bc07c82b908272b8
Uziel Shemesh <uziel110@gmail.com>
2020-03-08 17:59:25 +0200

    first commit

--- src/primitives/Coordinate.java
+++ src/primitives/Coordinate.java
@@ -5,7 +5,8 @@ import static primitives.Util.*;
 /**
  * Class Coordinate is the basic class representing a coordinate for Cartesian
  * coordinate system. The class is based on Util controlling the accuracy.
- * 
+ *
+ *
  * @author Dan Zilberstein
  */
 public final class Coordinate {
