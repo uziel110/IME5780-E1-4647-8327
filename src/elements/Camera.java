e6c36b71a9003a12aec70f5296edd315b9f8a9e5
Uziel Shemesh <uziel110@gmail.com>
2020-04-27 15:18:12 +0300

    end of recitation 4

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -1,9 +1,11 @@
 package elements;
 
-import com.sun.source.tree.BreakTree;
 import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
+
+import static primitives.Util.isZero;
+
 /**
  * class that implements Camera
  */
@@ -13,9 +15,10 @@ public class Camera {
 
     /**
      * constructor of Camera
+     *
      * @param location Point3D on the camera
-     * @param vTo vector direction forward
-     * @param vUp vector direction up
+     * @param vTo      vector direction forward
+     * @param vUp      vector direction up
      * @throws IllegalArgumentException if vTo and vUp aren't orthogonal
      */
     public Camera(Point3D location, Vector vTo, Vector vUp) {
@@ -31,51 +34,77 @@ public class Camera {
 
     /**
      * return ray from point location on the camera and goes through a certain pixel on the screen
-     * @param nX number of pixels in a row
-     * @param nY number of pixels in the column
-     * @param j pixel row index
-     * @param i pixel column index
+     *
+     * @param nX             number of pixels in a row
+     * @param nY             number of pixels in the column
+     * @param j              pixel row index
+     * @param i              pixel column index
      * @param screenDistance distance between location point and the screen
-     * @param screenWidth width of the screen
-     * @param screenHeight height of the screen
+     * @param screenWidth    width of the screen
+     * @param screenHeight   height of the screen
      * @return ray from point location on the camera and goes through a certain pixel on the screen
      */
     public Ray constructRayThroughPixel(int nX, int nY,
                                         int j, int i, double screenDistance,
                                         double screenWidth, double screenHeight) {
-        Point3D pc = _location.add(_vTo.scale(screenDistance));
+        Point3D pc = new Point3D(_location);
+        if (!isZero(screenDistance))
+            pc = _location.add(_vTo.scale(screenDistance));
+
+        // to avoid zero view plane
+        if (nY == 0 || nX == 0)
+            return null;
         double ry = screenHeight / nY;
         double rx = screenWidth / nX;
 
-        Point3D pij = pc.add(_vRight.scale((j - ((nX - 1) / 2.0)) * rx).subtract(_vUp.scale((i - ((nY - 1) / 2.0))* ry)));
+        Point3D pij;
+        boolean y = isZero(j - (nX - 1) / 2.0);
+        boolean x = isZero(i - (nY - 1) / 2.0);
+        if (y && x)
+            pij = new Point3D(pc);
+        else if (y)
+            pij = pc.add((Point3D.ZERO).subtract((_vUp.scale((i - ((nY - 1) / 2.0)) * ry)).getEnd()));
+        else if (x)
+            pij = pc.add(_vRight.scale((j - ((nX - 1) / 2.0)) * rx));
+        else
+            pij = pc.add(_vRight.scale((j - ((nX - 1) / 2.0)) * rx).subtract(_vUp.scale((i - ((nY - 1) / 2.0)) * ry)));
+
+        // pij is always not equal to _location
         Vector vij = pij.subtract(_location);
 
-        return new Ray(_location,vij.normalized());
+        return new Ray(_location, vij.normalized());
     }
 
     /**
      * return a point Location on the camera
+     *
      * @return point Location on the camera
      */
     public Point3D getLocation() {
         return _location;
     }
+
     /**
      * return a vector direction forward
+     *
      * @return vector direction forward
      */
     public Vector getvTo() {
         return _vTo;
     }
+
     /**
      * return a vector direction up
+     *
      * @return vector direction up
      */
     public Vector getvUp() {
         return _vUp;
     }
+
     /**
      * return a vector direction right
+     *
      * @return vector direction right
      */
     public Vector getvRight() {
---
+++ src/unittests/elements/IntegrationTests.java
@@ -0,0 +1,214 @@
+package elements;
+
+import geometries.Plane;
+import geometries.Sphere;
+import geometries.Triangle;
+import org.junit.Test;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class IntegrationTests {
+
+    List<Point3D> tests;
+
+    /**
+     * Test method for integration of sphere with the camera
+     */
+    @Test
+    public void SphereIntegrationTest() {
+        // TC01: view plane 3X3 sphere radius 1 (2 intersections)
+        Sphere sphere = new Sphere(1, new Point3D(0, 0, 3));
+        Camera camera = new Camera(new Point3D(0, 0, 0), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        List<Point3D> result = new ArrayList<Point3D>();
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = sphere.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 2, result.size());
+
+        // TC02: view plane 3X3 sphere radius 2.5 (18 intersections)
+        sphere = new Sphere(2.5, new Point3D(0, 0, 2.5));
+        camera = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = sphere.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 18, result.size());
+
+        // TC03: view plane 3X3 sphere radius 2 (10 intersections)
+        sphere = new Sphere(2, new Point3D(0, 0, 2));
+        camera = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = sphere.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 10, result.size());
+
+        // TC04: view plane 3X3 sphere radius 4 (9 intersections)
+        sphere = new Sphere(4, new Point3D(0, 0, 2));
+        camera = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = sphere.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 9, result.size());
+
+        // TC05: view plane 3X3 sphere radius 0.5 (0 intersections)
+        sphere = new Sphere(0.5, new Point3D(0, 0, -1));
+        camera = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = sphere.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 0, result.size());
+    }
+
+    /**
+     * Test method for integration of plane with the camera
+     */
+    @Test
+    public void PlaneIntegrationTest() {
+        // TC01: view plane 3X3 plane is parallel to view plane (9 intersections)
+        Plane plane = new Plane( new Point3D(0, 0, 3),new Vector(0,0,1));
+        Camera camera = new Camera(new Point3D(0, 0, 0), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        List<Point3D> result = new ArrayList<Point3D>();
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = plane.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 9, result.size());
+
+        // TC02: view plane 3X3 plane is in inclined to view plane (9 intersections)
+        plane = new Plane( new Point3D(0, 0, 3),new Vector(0,1,2));
+        camera = new Camera(new Point3D(0, 0, 0), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = plane.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 9, result.size());
+
+        // TC03: view plane 3X3 plane is in inclined to view plane (6 intersections)
+        plane = new Plane( new Point3D(0, 0, 3),new Vector(0,1,1));
+        camera = new Camera(new Point3D(0, 0, 0), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = plane.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 6, result.size());
+
+        // TC04: view plane 3X3 plane is orthogonal to view plane (3 intersections)
+        plane = new Plane( new Point3D(0, 3, 0),new Vector(0,1,0));
+        camera = new Camera(new Point3D(0, 0, 0), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = plane.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 3, result.size());
+
+        // TC05: view plane 3X3 plane is in inclined to view plane (0 intersections)
+        plane = new Plane( new Point3D(0, 0, -3),new Vector(0,1,1));
+        camera = new Camera(new Point3D(0, 0, 0), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = plane.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 0, result.size());
+    }
+
+    /**
+     * Test method for integration of triangle with the camera
+     */
+    @Test
+    public void TriangleIntegrationTest() {
+
+        // TC01: view plane 3X3 triangle is parallel to view plane (1 intersections)
+        Triangle triangle = new Triangle( new Point3D(1, 1, 2),new Point3D(-1,1,2),new Point3D(0,-1,2));
+        Camera camera = new Camera(new Point3D(0, 0, 0), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        List<Point3D> result = new ArrayList<Point3D>();
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = triangle.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 1, result.size());
+
+        // TC02: view plane 3X3 triangle is parallel to view plane (2 intersections)
+        triangle = new Triangle( new Point3D(1, 1, 2),new Point3D(-1,1,2),new Point3D(0,-20,2));
+        camera = new Camera(new Point3D(0, 0, 0), new Vector(0, 0, 1), new Vector(0, -1, 0));
+
+        result = new ArrayList<Point3D>();
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                tests = triangle.findIntersections(camera.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (tests != null)
+                    result.addAll(tests);
+            }
+        }
+        assertEquals("Wrong number of intersections", 2, result.size());
+    }
+}
