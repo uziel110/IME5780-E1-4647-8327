76b018cd0dbbb21bcd9b0a118a3f4c6e1b272226
arilazar <57072150+arilazar@users.noreply.github.com>
2020-05-03 15:11:25 +0300

    before marge javadoc

--- src/parser/SaxHandler.java
+++ src/parser/SaxHandler.java
@@ -20,6 +20,10 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 
+/**
+ * create Render Scene and Image from xml file
+ * if file incorrect then render = null
+ */
 public class SaxHandler extends DefaultHandler {
 
     Render render = null;
@@ -35,11 +39,13 @@ public class SaxHandler extends DefaultHandler {
      */
     public SaxHandler(String sceneXmlFileName) {
         _sceneXmlFileName = sceneXmlFileName;
-        parseDocument();
     }
 
-    private void parseDocument() {
-        // parse
+    /**
+     *
+     * @return render
+     */
+    public Render parseDocument() {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         try {
             SAXParser parser = factory.newSAXParser();
@@ -51,99 +57,100 @@ public class SaxHandler extends DefaultHandler {
         } catch (IOException e) {
             System.out.println("IO error");
         }
+        return render;
     }
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) {
         double[] doubleArr;
-        try {
 
-            if (qName.equalsIgnoreCase("Scene")) {
+        if (qName.equalsIgnoreCase("Scene")) {
 
-                _scene = new Scene("scene import from XML");
-                doubleArr = getDoubleArray(attributes, "background-color");
+            _scene = new Scene("scene import from XML");
+            doubleArr = getDoubleArray(attributes, "background-color");
 
-                _scene.setBackground(new Color(doubleArr[0], doubleArr[1], doubleArr[2]));
-                String distance = attributes.getValue("screen-distance");
-                _scene.setDistance(Integer.parseInt(distance));
-            }
+            _scene.setBackground(new Color(doubleArr[0], doubleArr[1], doubleArr[2]));
+            String distance = attributes.getValue("screen-distance");
+            _scene.setDistance(Integer.parseInt(distance));
+        }
 
-            if (qName.equalsIgnoreCase("camera")) {
+        if (qName.equalsIgnoreCase("camera")) {
 
-                doubleArr = getDoubleArray(attributes, "P0");
-                Point3D point = new Point3D(doubleArr[0], doubleArr[1], doubleArr[2]);
+            doubleArr = getDoubleArray(attributes, "P0");
+            Point3D point = new Point3D(doubleArr[0], doubleArr[1], doubleArr[2]);
 
-                doubleArr = getDoubleArray(attributes, "Vto");
-                Vector vecTo = new Vector(doubleArr[0], doubleArr[1], doubleArr[2]);
+            doubleArr = getDoubleArray(attributes, "Vto");
+            Vector vecTo = new Vector(doubleArr[0], doubleArr[1], doubleArr[2]);
 
-                doubleArr = getDoubleArray(attributes, "Vup");
-                Vector vecUp = new Vector(doubleArr[0], doubleArr[1], doubleArr[2]);
+            doubleArr = getDoubleArray(attributes, "Vup");
+            Vector vecUp = new Vector(doubleArr[0], doubleArr[1], doubleArr[2]);
 
-                _scene.setCamera(new Camera(point, vecTo, vecUp));
-            }
+            _scene.setCamera(new Camera(point, vecTo, vecUp));
+        }
 
-            if (qName.equalsIgnoreCase("ambient-light")) {
+        if (qName.equalsIgnoreCase("ambient-light")) {
 
-                doubleArr = getDoubleArray(attributes, "color");
-                _scene.setAmbientLight(new AmbientLight(new Color(doubleArr[0], doubleArr[1], doubleArr[2]), 1));
-            }
+            doubleArr = getDoubleArray(attributes, "color");
+            _scene.setAmbientLight(new AmbientLight(new Color(doubleArr[0], doubleArr[1], doubleArr[2]), 1));
+        }
 
-            if (qName.equalsIgnoreCase("sphere")) {
+        if (qName.equalsIgnoreCase("sphere")) {
 
-                double radius = Double.parseDouble(attributes.getValue("radius"));
-                _scene.addGeometries(new Sphere(radius, getPointFromStr(attributes, "center")));
+            double radius = Double.parseDouble(attributes.getValue("radius"));
+            _scene.addGeometries(new Sphere(radius, getPointFromStr(attributes, "center")));
 
-            }
-            if (qName.equalsIgnoreCase("triangle")) {
+        }
+        if (qName.equalsIgnoreCase("triangle")) {
 
-                Point3D p0 = getPointFromStr(attributes, "p0");
-                Point3D p1 = getPointFromStr(attributes, "p1");
-                Point3D p2 = getPointFromStr(attributes, "p2");
+            Point3D p0 = getPointFromStr(attributes, "p0");
+            Point3D p1 = getPointFromStr(attributes, "p1");
+            Point3D p2 = getPointFromStr(attributes, "p2");
 
-                _scene.addGeometries(new Triangle(p0, p1, p2));
-            }
+            _scene.addGeometries(new Triangle(p0, p1, p2));
+        }
 
-            if (qName.equalsIgnoreCase("plane")) {
+        if (qName.equalsIgnoreCase("plane")) {
 
-                Point3D p0 = getPointFromStr(attributes, "p0");
-                Point3D p1 = getPointFromStr(attributes, "p1");
-                Point3D p2 = getPointFromStr(attributes, "p2");
+            Point3D p0 = getPointFromStr(attributes, "p0");
+            Point3D p1 = getPointFromStr(attributes, "p1");
+            Point3D p2 = getPointFromStr(attributes, "p2");
 
-                _scene.addGeometries(new Plane(p0, p1, p2));
-            }
+            _scene.addGeometries(new Plane(p0, p1, p2));
+        }
 
-            if (qName.equalsIgnoreCase("image")) {
-                double screenWidth, screenHeight;
-                int nX, nY;
-                screenWidth = Double.parseDouble(attributes.getValue("screen-width"));
-                screenHeight = Double.parseDouble(attributes.getValue("screen-height"));
-                nX = Integer.parseInt(attributes.getValue("Nx"));
-                nY = Integer.parseInt(attributes.getValue("Ny"));
-                image = new ImageWriter("image from XML",
-                        screenWidth, screenHeight, nX, nY);
-            }
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-            return;
+        if (qName.equalsIgnoreCase("image")) {
+
+            double screenWidth = Double.parseDouble(attributes.getValue("screen-width"));
+            double screenHeight = Double.parseDouble(attributes.getValue("screen-height"));
+            int nX = Integer.parseInt(attributes.getValue("Nx"));
+            int nY = Integer.parseInt(attributes.getValue("Ny"));
+            image = new ImageWriter("image from XML",
+                    screenWidth, screenHeight, nX, nY);
         }
     }
 
-    public Render getRender() {
-        return render;
-    }
-
+    /**
+     * convert string of double values into double array
+     * @param attributes the attributes attached to the element
+     * @param str key name
+     * @return new double array
+     */
     private double[] getDoubleArray(Attributes attributes, String str) {
         String num = attributes.getValue(str);
         String[] number = num.split(" ");
-        double[] doubles = new double[]{Double.parseDouble(number[0]),
-                Double.parseDouble(number[1]),
-                Double.parseDouble(number[2])};
-        return doubles;
+        return new double[]{Double.parseDouble(number[0]), Double.parseDouble(number[1]), Double.parseDouble(number[2])};
     }
 
+    /**
+     * convert string of double values to point3D
+     * @param attributes the attributes attached to the element
+     * @param str key name
+     * @return new Point3D
+     */
     private Point3D getPointFromStr(Attributes attributes, String str) {
-        double[] number = getDoubleArray(attributes, str);
-        return new Point3D((number[0]), (number[1]), (number[2]));
+        String num = attributes.getValue(str);
+        String[] number = num.split(" ");
+        return new Point3D(Double.parseDouble(number[0]), Double.parseDouble(number[1]), Double.parseDouble(number[2]));
     }
 
     @Override
