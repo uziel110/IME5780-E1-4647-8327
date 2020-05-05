1bcf9e3848a1c92fbf8b68b2f43e363d771e7833
arilazar <57072150+arilazar@users.noreply.github.com>
2020-05-05 17:54:40 +0300

    start recitation6

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,17 +1,25 @@
 package geometries;
 
+import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector;
 
 /**
  * interface for geometries shapes
  */
-public interface Geometry extends Intersectable {
+public abstract class Geometry implements Intersectable {
+
+    protected Color _emmission;
+
+    public Color get_emmission() {
+        return _emmission;
+    }
+
     /**
      * return normal to the geometry shape from the received point
      *
      * @param point point on the surface
      * @return normal to the geometry shape from the received point
      */
-    Vector getNormal(Point3D point);
+    public abstract Vector getNormal(Point3D point);
 }
